bef15b25547247c420fa4b49c8b661f38a8f0533
Kyu hyunSung <dmp100@naver.com>
2024-10-01 09:09:48 +0900

    1주차_dra.2
    감정우표 만들기
    - Figma에 있는 단일 화면을 구성해보기
    - LinearLayout, ConstraintLayout등 본인이 적절하다고 판단되는 것으로 화면을 구현하고나서 어떤 Layout이 더 효율적인지 정리해보기
    - 감정우표 옵션을 클릭했을 때 임의의 다음 화면으로 넘어갈 수 있도록 설정해보기

---
+++ app/src/main/java/com/example/hsu_umc_android_7th/AngerFragment.kt
@@ -0,0 +1,19 @@
+package com.example.hsu_umc_android_7th
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+
+class AngerFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // fragment_excited 레이아웃을 inflate하여 반환
+        return inflater.inflate(R.layout.fragment_anger, container, false)
+    }
+}
---
+++ app/src/main/java/com/example/hsu_umc_android_7th/NormalFragment.kt
@@ -0,0 +1,19 @@
+package com.example.hsu_umc_android_7th
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+
+class NormalFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        return inflater.inflate(R.layout.fragment_normal, container, false)
+    }
+}
